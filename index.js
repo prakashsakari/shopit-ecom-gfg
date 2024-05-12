6011898ce04006a18a4de0f0b159c12c1b2b24a2
Prakash Sakari <sakariprakash@gmail.com>
2024-05-12 18:58:42 +0530

    enabled cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
+const cors = require('cors');
 const PORT = 3000;
 const connectDB = require("./config/dbconfig");
 const {
@@ -16,6 +17,7 @@ const {
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 connectDB();
 
